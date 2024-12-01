a419c7f24d5a1acbb4fa585e44e8a9009faa666f
NapolskikhM <m9217203819@rambler.ru>
2024-12-01 21:25:21 +0300

    Add files via upload

---
+++ JWTaccess/app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class RestrictedView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response(data={"message": "You have access to this restricted content."})
+
+
+def main_page(request):
+    return render(request, 'mainpage.html')
+
+
